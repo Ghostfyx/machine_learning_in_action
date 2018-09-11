d1f91274cca5d0420cfdb8b60a7ab117831e0e86
MadnessMan <1078101544@qq.com>
2018-09-11 16:58:22 +0800

    KNN数据处理完毕

--- KNN/knn.py
+++ KNN/knn.py
@@ -13,11 +13,18 @@ from pprint import pprint
 
 from dataformate import get_knn_data
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
 
 filepath = '../dataset/KNNData.txt'
 name = ['fly distance', 'play time', 'ice cream', 'like level']
 df = get_knn_data(filepath, names=name)
-labelset = set(df.iloc[:, -1])
-
+X = df.iloc[:, :-1].values
+y = df.iloc[:, -1].values
+# 对数据进行标准化处理
+sc = StandardScaler()
+X_std = sc.fit_transform(X)
 # 使用matplotlab绘制散点图
-plt.scatter()
+plt.scatter(X[y==1, 0], X[y==1, 1], color='red', marker='^', alpha=0.5)
+plt.scatter(X[y==2, 0], X[y==2, 1], color='blue', marker='*', alpha=0.5)
+plt.scatter(X[y==3, 0], X[y==3, 1], color='green', marker='o', alpha=0.5)
+plt.show()
